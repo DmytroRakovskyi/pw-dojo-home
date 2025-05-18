f9e24fcc594df0bd4d0d8bfe21825bcd871c71b5
DmytroRakovskyi <dmytriy.rakovskiy@gmail.com>
2025-05-18 14:19:15 +0200

    Refactored selectors for coffe-cart spec

--- test/coffee-cart/coffee-cart-functionality.spec.ts
+++ test/coffee-cart/coffee-cart-functionality.spec.ts
@@ -17,9 +17,9 @@ test("AQA-7 adding one element to the cart", { tag: ["@smoke-coffee"] }, async (
   await expect(page.locator('[data-test="Espresso"]')).toContainText("espresso");
   await page.locator('[data-test="Espresso"]').click();
   await expect(page.locator('[aria-label*="Cart"]')).toContainText("cart (1)");
-  await page.locator('button[data-test="checkout"].pay').hover();
-  await expect(page.locator('[class="cart-preview show"]')).toBeVisible(); //
-  await page.locator('button[data-test="checkout"].pay').click();
+  await page.locator('button[data-test="checkout"]').hover();
+  await expect(page.locator('[class*="cart-pr"]')).toBeVisible();
+  await page.locator('button[data-test="checkout"]').click();
   await expect(page.locator(".modal .modal-content")).toBeVisible();
   await page.locator('button[class="close"]').click();
   await expect(page.locator(".modal .modal-content")).toBeHidden();
@@ -39,11 +39,9 @@ test("AQA-9 increase/decrease element amount via +-", { tag: ["@smoke-coffee"] }
   await page.locator('a[href*="/cart"]').click();
   await expect(page).toHaveURL("https://coffee-cart.app/cart");
   await expect(page.title()).resolves.toContain("Coffee cart");
-
-  await expect(page.locator('li[class="list-item"] div').filter({ hasText: /^Espresso$/ })).toBeVisible();
-  await page.locator('button[data-v-8965af83][aria-label="Add one Espresso"]').click();
+  await page.getByRole('button', { name: 'Add' }).click();
   await expect(page.locator('a[href*="/cart"]')).toContainText("cart (2)");
-  await page.locator('button[data-v-8965af83][aria-label="Remove one Espresso"]').dblclick();
+  await page.getByRole('button', { name: 'Remove all' }).click()
   await expect(page.locator('a[href*="/cart"]')).toContainText("cart (0)");
 });
 
@@ -52,8 +50,6 @@ test("AQA-10 remove element from cart", { tag: ["@smoke-coffee"] }, async ({ pag
   await page.locator('a[href*="/cart"]').click();
   await expect(page).toHaveURL("https://coffee-cart.app/cart");
   await expect(page.title()).resolves.toContain("Coffee cart");
-
-  await expect(page.locator('li[class="list-item"] div').filter({ hasText: /^Espresso$/ })).toBeVisible();
-  await page.locator('button[data-v-8965af83][aria-label="Remove all Espresso"]').dblclick(); // to change 
-  await expect(page.locator(".list p")).toHaveText("No coffee, go add some.");
+ await page.getByRole('button', { name: 'Remove all' }).click()
+  await expect(page.locator(".list p")).toContainText("No coffee");
 });
